6e4b9826482b8ef2c9e3dcf8020df0d4e8e039f7
@TNB142 <20520142@gm.uit.edu.vn>
2022-11-23 16:29:55 +0700

    khanh update

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -57,6 +57,7 @@ export function Header() {
               style={{
                 width: "2.5rem",
               }}
+              
               onClick={openCart}
             />
             {cartQuantity > 0 && (
