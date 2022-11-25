1b56affae0bcd545a9506491a0800e2c035f21b7
@TNB142 <20520142@gm.uit.edu.vn>
2022-11-25 22:08:53 +0700

    update shopping cart

--- src/component/menu/cartitem/CartItem.tsx
+++ src/component/menu/cartitem/CartItem.tsx
@@ -1,54 +1,75 @@
-import { Button, Stack } from "react-bootstrap"
-import { useShoppingCart } from "../../../context/shoppingCartContext"
-import menuItems from "../../../data/items.json"
+import { Button, Stack } from "react-bootstrap";
+import { useShoppingCart } from "../../../context/shoppingCartContext";
+import menuItems from "../../../data/items.json";
+import "./style.css";
 
 type CartItemProps = {
-    id: number
-    quantity: number
-}
+  id: number;
+  quantity: number;
+};
 
 export function CartItem({ id, quantity }: CartItemProps) {
-    const { decreaseCartQuantity } = useShoppingCart()
-    const item = menuItems.find(i => i.id === id)
-    if (item == null) return null
+  const {
+    removeFromCart,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    getItemQuantity,
+    cartQuantity,
+  } = useShoppingCart();
+  const item = menuItems.find((i) => i.id === id);
+  if (item == null) return null;
+  const increaseClick = (event: any) => {
+    // 👇️ refers to the image element
+    console.log(event.target);
 
+    console.log("increase item");
+    if (cartQuantity < 3) increaseCartQuantity(item.id);
+    else alert("Chỉ được thêm tối đa 3 món");
+  };
+  const decreaseClick = (event: any) => {
+    // 👇️ refers to the image element
+    console.log(event.target);
 
-
-    return (
-        <Stack direction="horizontal" gap={2}
-            className="d-flex align-items-center">
-            <img src={item.imgUrl}
-                style={{
-                    width: "125px",
-                    height: "75px",
-                    objectFit: "cover"
-                }} />
-            <div className="me-auto">
-                <div>
-                    {item.name} {" "}
-                    <div>
-                        (<span
-                            className="text-muted"
-                            style={{
-                                fontSize: "1rem"
-                            }}>
-                            Số lượng: {quantity} phần
-                        </span>
-                        )
-                    </div>
-                </div>
-            </div>
-            <Button
-                variant="danger"
-                size="sm"
-                // className="bg-danger"
-                onClick={() => decreaseCartQuantity(item.id)}
-            >
-                <img
-                alt=""
-                src="../svg/delete.svg"
-                />
-            </Button>
-        </Stack>
-    )
+    console.log("decrease item");
+    decreaseCartQuantity(item.id);
+  };
+  return (
+    <Stack
+      direction="horizontal"
+      gap={4}
+      className="CartCard d-flex align-items-center"
+    >
+      <div>
+        <img src={item.imgUrl} className="imgDish" />
+      </div>
+      <Stack
+        direction="vertical"
+        gap={2}
+        className="food_name_container d-flex justify-content-center"
+      >
+        <div className="d-flex flex-row justify-content-end trash_item">
+          <img
+            alt=""
+            src="../public/svg/trash-can-empty.svg"
+            onClick={() => removeFromCart(item.id)}
+          />
+        </div>
+        <div>
+          <div className="w-100">
+            <div className="nameDish_text">{item.name}</div>
+            <div className="typeDish_text">{item.type}</div>
+          </div>
+        </div>
+        <div className="h-100 d-flex justify-content-end">
+          <button className="button_container_1" onClick={decreaseClick}>
+            -
+          </button>
+          <div className="quantity_text">{getItemQuantity(item.id)}</div>
+          <button className="button_container_2" onClick={increaseClick}>
+            +
+          </button>
+        </div>
+      </Stack>
+    </Stack>
+  );
 }
