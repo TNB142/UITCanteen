68d083ef4b2807f1bfd390ae60d982469fea4d22
cptdtbk <20521444@gm.uit.edu.vn>
2022-12-01 01:09:52 +0700

    logout

--- src/pages/logout/logout.tsx
+++ src/pages/logout/logout.tsx
@@ -15,7 +15,7 @@ export function Logout() {
     }, [navigate]);
 
     Axios.defaults.withCredentials = true;
-    Axios.get("https://uitcanteen-backend.herokuapp.com/logout").then((response) => {
+    Axios.get("https:/uitcanteen-backend.herokuapp.com/logout").then((response) => {
         if (response.data.message) {
             toast(response.data.message);
         }
