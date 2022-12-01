36202e08a0641dd9738b197a2bc2fba873f70be8
@TNB142 <20520142@gm.uit.edu.vn>
2022-12-01 16:47:07 +0700

    fix sign out

--- src/pages/logout/logout.tsx
+++ src/pages/logout/logout.tsx
@@ -8,94 +8,96 @@ import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export function Logout() {
-    const navigate = useNavigate();
-    const loginButton = useCallback(() => {
-        navigate("/Login");
-        // window.localStorage.setItem("isLoggedIn","false");
-        window.localStorage.removeItem("isLoggedIn")
-        // window.localStorage.removeItem("userData")
-        console.log(window.localStorage.getItem("isLoggedIn"));
-        window.location.reload();
-    }, [navigate]);
+  const navigate = useNavigate();
+  const loginButton = useCallback(() => {
+    // window.localStorage.setItem("isLoggedIn","false");
+    window.localStorage.removeItem("isLoggedIn");
+    // window.localStorage.removeItem("userData")
+    console.log(window.localStorage.getItem("isLoggedIn"));
+    navigate("/Login");
+    window.location.reload();
+  }, [navigate]);
 
-    Axios.defaults.withCredentials = true;
-    Axios.get("https://uitcanteen-backend.herokuapp.com/logout").then((response) => {
-        if (response.data.message) {
-            toast(response.data.message);
-        }
-    });
-    return (
-        <>
-        <ToastContainer />
-            <Container className="pt-5" id="login">
-                <Row>
-                    <Col className="mb-5">
-                        <Stack>
-                            <h1 className="login_text">Trải nghiệm đặt món online tại</h1>
-                            <h1 className="CanteenUIT_login_text">Canteen UIT</h1>
-                            <div>
-                                <img
-                                    alt=""
-                                    src="../imgs/UIT_CANTEEN.png"
-                                    style={{
-                                        width: "542px",
-                                    }}
-                                />
-                            </div>
-                        </Stack>
-                    </Col>
-                    <Col
-                        // className="bg-danger"
-                        className="d-flex flex-row justify-content-center align-items-start"
-                    // style={{
-                    //   paddingLeft:"10%"
-                    // }}
-                    >
-                        <div className="d-flex flex-column justify-content-center align-items-start">
-                            <h1 className="mb-3 mg-3 login_head">Đăng Xuất</h1>
+  Axios.defaults.withCredentials = true;
+  Axios.get("https://uitcanteen-backend.herokuapp.com/logout").then(
+    (response) => {
+      if (response.data.message) {
+        toast(response.data.message);
+      }
+    }
+  );
+  return (
+    <>
+      <ToastContainer />
+      <Container className="pt-5" id="login">
+        <Row>
+          <Col className="mb-5">
+            <Stack>
+              <h1 className="login_text">Trải nghiệm đặt món online tại</h1>
+              <h1 className="CanteenUIT_login_text">Canteen UIT</h1>
+              <div>
+                <img
+                  alt=""
+                  src="../imgs/UIT_CANTEEN.png"
+                  style={{
+                    width: "542px",
+                  }}
+                />
+              </div>
+            </Stack>
+          </Col>
+          <Col
+            // className="bg-danger"
+            className="d-flex flex-row justify-content-center align-items-start"
+            // style={{
+            //   paddingLeft:"10%"
+            // }}
+          >
+            <div className="d-flex flex-column justify-content-center align-items-start">
+              <h1 className="mb-3 mg-3 login_head">Đăng Xuất</h1>
 
-                            <div
-                                className="d-flex flex-row align-items-center mb-3"
-                                style={{
-                                    width: "100%",
-                                }}
-                            >
-                                <div
-                                    style={{
-                                        width: "100%",
-                                        height: "3px",
-                                        backgroundColor: "#3F3F3F",
-                                        borderRadius: "2px 2px",
-                                    }}
-                                ></div>
-                                <span
-                                    className="text-center font-weight-bold donthaveAcount_text"
-                                    style={{
-                                        width: "150%",
-                                    }}
-                                >
-                                    Muốn đăng nhập lại khum?
-                                </span>
-                                <div
-                                    // className="text-center"
-                                    style={{
-                                        width: "100%",
-                                        height: "3px",
-                                        backgroundColor: "#3F3F3F",
-                                        borderRadius: "2px 2px",
-                                    }}
-                                ></div>
-                            </div>
-                            <button
-                                className=" text-center button_login_page"
-                                onClick={loginButton}
-                            >
-                                <div className="login_page_Text"> Đăng Nhập </div>
-                            </button>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+              <div
+                className="d-flex flex-row align-items-center mb-3"
+                style={{
+                  width: "100%",
+                }}
+              >
+                <div
+                  style={{
+                    width: "100%",
+                    height: "3px",
+                    backgroundColor: "#3F3F3F",
+                    borderRadius: "2px 2px",
+                  }}
+                ></div>
+                <span
+                  className="text-center font-weight-bold donthaveAcount_text"
+                  style={{
+                    width: "150%",
+                  }}
+                >
+                  Muốn đăng nhập lại khum?
+                </span>
+                <div
+                  // className="text-center"
+                  style={{
+                    width: "100%",
+                    height: "3px",
+                    backgroundColor: "#3F3F3F",
+                    borderRadius: "2px 2px",
+                  }}
+                ></div>
+              </div>
+              <button
+                className=" text-center button_login_page"
+                onClick={loginButton}
+              >
+                <div className="login_page_Text"> Đăng Nhập </div>
+              </button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
